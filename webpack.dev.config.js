326de84d32d1a354863686a4376ee46051992936
L <376693576@qq.com>
2020-06-16 14:42:07 +0800

    update

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,8 +4,8 @@ const path = require("path"); // 获取绝对路径用
 const webpack = require("webpack"); // webpack核心
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // 动态生成html插件
-const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
-const tsImportPluginFactory = require("ts-import-plugin");
+const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin"); // 使用day.js替代antd中的moment.js
+const tsImportPluginFactory = require("ts-import-plugin"); // 用于ts版本的按需加载
 const webpackbar = require("webpackbar");
 
 const PUBLIC_PATH = "/"; // 基础路径
@@ -118,7 +118,7 @@ module.exports = {
   plugins: [
     new webpackbar(),
     new webpack.HotModuleReplacementPlugin(), // 热更新插件
-    new AntdDayjsWebpackPlugin(), // dayjs 替代 momentjs
+    new AntdDayjsWebpackPlugin(),
     new webpack.DefinePlugin({
       "process.env": JSON.stringify({
         PUBLIC_URL: PUBLIC_PATH,
@@ -140,14 +140,14 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       // 根据模板插入css/js等生成最终HTML
-      filename: "index.html", //生成的html存放路径，相对于 output.path
+      filename: "index.html", // 生成的html存放路径，相对于 output.path
       favicon: "./public/favicon.png", // 自动把根目录下的favicon.ico图片加入html
-      template: "./public/index.html", //html模板路径
+      template: "./public/index.html", // html模板路径
       inject: true, // 是否将js放在body的末尾
     }),
   ],
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".less", ".css", ".wasm"], //后缀名自动补全
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".less", ".css", ".wasm"], // 后缀名自动补全
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
