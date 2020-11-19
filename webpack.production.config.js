4f5674aa583aec2fef7e32cda886f2e43b5831c4
Logic <376693576@qq.com>
2020-11-19 15:27:23 +0800

    update path

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -176,7 +176,7 @@ module.exports = {
       registerServiceWorker: `<script>
         if ("serviceWorker" in navigator) {
           window.addEventListener("load", () => {
-            navigator.serviceWorker.register("/service-worker.js");
+            navigator.serviceWorker.register("./service-worker.js");
           });
         }
       </script>`,
